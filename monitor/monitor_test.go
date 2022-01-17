98829531250c2a7d4ce5561eae22449efb63c2a5
t_max <1172915550@qq.com>
2022-01-17 11:17:05 +0800

    [TD-12847]<test>: improve monitor function test

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -1,23 +1,149 @@
-package monitor
+package monitor_test
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"runtime"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/process"
 	"github.com/stretchr/testify/assert"
 	"github.com/taosdata/taosadapter/config"
+	"github.com/taosdata/taosadapter/controller/rest"
+	"github.com/taosdata/taosadapter/db"
+	"github.com/taosdata/taosadapter/monitor"
 )
+
+var router *gin.Engine
 
 func TestMain(m *testing.M) {
 	config.Init()
+	db.PrepareConnection()
+	gin.SetMode(gin.ReleaseMode)
+	router = gin.New()
+	router.Use(func(context *gin.Context) {
+		context.Set("currentID", uint32(0))
+	})
+	var ctl rest.Restful
+	ctl.Init(router)
+	m.Run()
 }
+
+// @author: xftan
+// @date: 2022/1/17 11:14
+// @description: test monitor function
 func TestMonitor(t *testing.T) {
-	StartMonitor()
-	assert.False(t, QueryPaused())
-	assert.False(t, AllPaused())
-	config.Conf.Monitor.PauseQueryMemoryPercent = 0
-	config.Conf.Monitor.PauseAllMemoryPercent = 0
+	machineMemory, err := mem.VirtualMemoryWithContext(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	total := machineMemory.Total
+	p, err := process.NewProcess(int32(os.Getpid()))
+	processMemory, err := p.MemoryInfoWithContext(context.Background())
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	used := processMemory.RSS
+	currentPercent := 100 * (float64(used) / float64(total))
+	//+6%
+	size := int(float64(total) * 0.06)
+	//6% + 5% = 11%
+	size2 := int(float64(total) * 0.05)
+	config.Conf.Monitor.PauseQueryMemoryPercent = currentPercent + 5
+	config.Conf.Monitor.PauseAllMemoryPercent = currentPercent + 10
+	monitor.StartMonitor()
+	{
+		assert.False(t, monitor.QueryPaused())
+		assert.False(t, monitor.AllPaused())
+		w := httptest.NewRecorder()
+		body := strings.NewReader("show databases")
+		req, _ := http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+
+		w = httptest.NewRecorder()
+		body = strings.NewReader("create database if not exists t1")
+		req, _ = http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+	}
+
+	var a = make([]byte, size)
+	for i := 0; i < size; i++ {
+		a[i] = 1
+	}
 	time.Sleep(config.Conf.Monitor.CollectDuration)
-	assert.True(t, QueryPaused())
-	assert.True(t, AllPaused())
+	{
+		assert.True(t, monitor.QueryPaused())
+		assert.False(t, monitor.AllPaused())
+		w := httptest.NewRecorder()
+		body := strings.NewReader("show databases")
+		req, _ := http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 503, w.Code)
+
+		w = httptest.NewRecorder()
+		body = strings.NewReader("create database if not exists t1")
+		req, _ = http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+	}
+
+	b := make([]byte, size2)
+	for i := 0; i < size2; i++ {
+		b[i] = 1
+	}
+	time.Sleep(config.Conf.Monitor.CollectDuration)
+	{
+		assert.True(t, monitor.QueryPaused())
+		assert.True(t, monitor.AllPaused())
+		w := httptest.NewRecorder()
+		body := strings.NewReader("show databases")
+		req, _ := http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 503, w.Code)
+
+		w = httptest.NewRecorder()
+		body = strings.NewReader("create database if not exists t1")
+		req, _ = http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 503, w.Code)
+	}
+	assert.True(t, monitor.QueryPaused())
+	assert.True(t, monitor.AllPaused())
+	a = nil
+	b = nil
+	runtime.GC()
+	time.Sleep(config.Conf.Monitor.CollectDuration * 2)
+	{
+		assert.False(t, monitor.QueryPaused())
+		assert.False(t, monitor.AllPaused())
+		w := httptest.NewRecorder()
+		body := strings.NewReader("show databases")
+		req, _ := http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+
+		w = httptest.NewRecorder()
+		body = strings.NewReader("create database if not exists t1")
+		req, _ = http.NewRequest(http.MethodPost, "/rest/sql", body)
+		req.Header.Set("Authorization", "Taosd /KfeAzX/f9na8qdtNZmtONryp201ma04bEl8LcvLUd7a8qdtNZmtONryp201ma04")
+		router.ServeHTTP(w, req)
+		assert.Equal(t, 200, w.Code)
+	}
 }
