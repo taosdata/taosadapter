e1ccc0294e03aa56dd60c8cc08a3a6cbaf16c16a
sunpeng <sunpeng.dev@gmail.com>
2022-12-20 16:29:32 +0800

    fix test

--- controller/rest/table_vgid_test.go
+++ controller/rest/table_vgid_test.go
@@ -3,12 +3,23 @@ package rest
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestVgID(t *testing.T) {
+	//
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "/rest/vgid?db=test&table=test", nil)
+	body := strings.NewReader("create database if not exists test_vgid")
+	req, _ := http.NewRequest(http.MethodPost, "/rest/sql", body)
+	req.Header.Set("Authorization", "Basic:cm9vdDp0YW9zZGF0YQ==")
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest(http.MethodGet, "/rest/vgid?db=test_vgid&table=test", nil)
 	req.Header.Set("Authorization", "Basic:cm9vdDp0YW9zZGF0YQ==")
 	router.ServeHTTP(w, req)
 	if w.Code != http.StatusOK {
