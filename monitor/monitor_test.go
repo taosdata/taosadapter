ac0f7688960319ef8e1297536f6d5f41058b14e0
sunpeng <sunpeng.dev@gmail.com>
2023-05-09 18:31:44 +0800

    fix test

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -228,10 +228,12 @@ func TestWriteLog(t *testing.T) {
 	}
 	time.Sleep(config.Conf.Monitor.WriteInterval * 2)
 	checkTables := []string{
-		"taosadapter_restful_http_total",
-		"taosadapter_restful_http_fail",
-		"taosadapter_restful_http_latency",
-		"taosadapter_system",
+		"taosadapter_restful_http_request_total",
+		"taosadapter_restful_http_request_fail",
+		"taosadapter_restful_http_request_in_flight",
+		"taosadapter_restful_http_request_summary_milliseconds",
+		"taosadapter_system_mem_percent",
+		"taosadapter_system_cpu_percent",
 	}
 	for _, table := range checkTables {
 		w := httptest.NewRecorder()
