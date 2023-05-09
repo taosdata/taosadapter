118a46fc69620f9f6a0eb2fc491a89eb37346a96
sunpeng <sunpeng.dev@gmail.com>
2023-05-09 15:16:08 +0800

    fix for monitor

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -177,9 +177,8 @@ func writeToTDLog() error {
 		return err
 	}
 	if !tableInitialized {
-		for i := 0; i < len(createList); i++ {
-			err = async.GlobalAsync.TaosExecWithoutResult(conn, createList[i], 0)
-			if err != nil {
+		for _, tb := range createList {
+			if err = async.GlobalAsync.TaosExecWithoutResult(conn, tb, 0); err != nil {
 				return err
 			}
 		}
@@ -235,7 +234,7 @@ func writeToTDLog() error {
 						}
 					}
 					inserts = append(inserts, fmt.Sprintf(" taosa_request_total_%s using taosadapter_restful_http_total tags('%s',%d,'%s','%s','%s') values('%s',%d)", subName, identity, statusCode, clientIP, requestMethod, requestUri, now, int(value)))
-					inserts = append(inserts, fmt.Sprintf(" taosa_request_total_%s using taosadapter_restful_http_request_total tags('%s',%s,'%s','%s','%d') values('%s',%f)", subName, clientIP, identity, requestMethod, requestUri, statusCode, now, value))
+					inserts = append(inserts, fmt.Sprintf(" taosa_http_request_total_%s using taosadapter_restful_http_request_total tags('%s','%s','%s','%s','%d') values('%s',%f)", subName, clientIP, identity, requestMethod, requestUri, statusCode, now, value))
 					log.TotalRequest.WithLabelValues(statusCodeStr, clientIP, requestMethod, requestUri).Sub(value)
 				}
 			}
@@ -270,7 +269,7 @@ func writeToTDLog() error {
 					}
 
 					inserts = append(inserts, fmt.Sprintf(" taosa_request_fail_%s using taosadapter_restful_http_fail tags('%s',%d,'%s','%s','%s') values('%s',%d)", subName, identity, statusCode, clientIP, requestMethod, requestUri, now, int(value)))
-					inserts = append(inserts, fmt.Sprintf(" taosa_request_fail_%s using taosadapter_restful_http_request_fail tags('%s',%s,'%s','%s','%d') values('%s',%f)", subName, clientIP, identity, requestMethod, requestUri, statusCode, now, value))
+					inserts = append(inserts, fmt.Sprintf(" taosa_http_request_fail_%s using taosadapter_restful_http_request_fail tags('%s','%s','%s','%s','%d') values('%s',%f)", subName, clientIP, identity, requestMethod, requestUri, statusCode, now, value))
 					log.FailRequest.WithLabelValues(statusCodeStr, clientIP, requestMethod, requestUri).Sub(value)
 				}
 			}
@@ -281,7 +280,7 @@ func writeToTDLog() error {
 				if value > 0 && !math.IsNaN(value) && !math.IsInf(value, 0) {
 					subName := calculateTableName(nil)
 					inserts = append(inserts, fmt.Sprintf(" taosa_request_in_flight_%s using taosadapter_restful_http_in_flight tags('%s') values('%s',%d)", subName, identity, now, int(value)))
-					inserts = append(inserts, fmt.Sprintf(" taosa_request_in_flight_%s using taosadapter_restful_http_request_in_flight tags('%s') values('%s',%f)", subName, identity, now, value))
+					inserts = append(inserts, fmt.Sprintf(" taosa_http_request_in_flight_%s using taosadapter_restful_http_request_in_flight tags('%s') values('%s',%f)", subName, identity, now, value))
 				}
 			}
 		case "taosadapter_restful_http_request_summary_milliseconds":
@@ -327,7 +326,7 @@ func writeToTDLog() error {
 				}
 				if !math.IsNaN(quantile1) {
 					inserts = append(inserts, fmt.Sprintf(" taosa_request_latency_%s using taosadapter_restful_http_latency tags('%s','%s','%s') values('%s',%f,%f,%f,%f,%f)", subName, identity, requestMethod, requestUri, now, quantile1, quantile2, quantile5, quantile9, quantile99))
-					inserts = append(inserts, fmt.Sprintf(" taosa_request_summary_%s using taosadapter_restful_http_request_summary_milliseconds tags('%s','%s','%s') values('%s',%f,%f,%f,%f,%f,%f,%f)", subName, identity, requestMethod, requestUri, now, quantile1, quantile2, quantile5, quantile9, quantile99, m.GetSummary().GetSampleCount(), m.GetSummary().GetSampleSum()))
+					inserts = append(inserts, fmt.Sprintf(" taosa_request_summary_%s using taosadapter_restful_http_request_summary_milliseconds tags('%s','%s','%s') values('%s',%f,%f,%f,%f,%f,%d,%f)", subName, identity, requestMethod, requestUri, now, quantile1, quantile2, quantile5, quantile9, quantile99, m.GetSummary().GetSampleCount(), m.GetSummary().GetSampleSum()))
 				}
 			}
 		case "taosadapter_system_cpu_percent":
@@ -343,7 +342,7 @@ func writeToTDLog() error {
 			memPercentValue = m.GetGauge().GetValue()
 			receiveSystemValue = true
 
-			inserts = append(inserts, fmt.Sprintf(" taos_system_mem_%s using taosadapter_system_mem_percent tags('%s') values('%s',%f)", calculateTableName(m.GetLabel()), identity, now, cpuPercentValue))
+			inserts = append(inserts, fmt.Sprintf(" taos_system_mem_%s using taosadapter_system_mem_percent tags('%s') values('%s',%f)", calculateTableName(m.GetLabel()), identity, now, memPercentValue))
 		}
 	}
 	if receiveSystemValue {
@@ -368,6 +367,7 @@ func writeToTDLog() error {
 		if builder.Len() > len(InsertStatement) {
 			err = async.GlobalAsync.TaosExecWithoutResult(conn, builder.String(), 0)
 			if err != nil {
+				logger.WithField("sql", builder.String()).Errorln("taos exec error", err)
 				return err
 			}
 		}
