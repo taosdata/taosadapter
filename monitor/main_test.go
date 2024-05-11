5261e49605f2102abcc9ba9669e0391c6c20f145
t_max <1172915550@qq.com>
2024-05-11 15:07:08 +0800

    ci: fix build TDengine and test taosAdapter
    (cherry picked from commit 73501c8fae1b9a0b4a80f041c5a3873cdcac497c)

---
+++ monitor/main_test.go
@@ -0,0 +1,37 @@
+package monitor_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"github.com/taosdata/taosadapter/v3/config"
+	"github.com/taosdata/taosadapter/v3/controller/ping"
+	"github.com/taosdata/taosadapter/v3/controller/rest"
+	"github.com/taosdata/taosadapter/v3/db"
+	"github.com/taosdata/taosadapter/v3/log"
+	"github.com/taosdata/taosadapter/v3/monitor"
+)
+
+var router *gin.Engine
+
+func TestMain(m *testing.M) {
+	viper.Set("monitor.disable", false)
+	viper.Set("monitor.disableCollectClientIP", false)
+	viper.Set("monitor.writeToTD", true)
+	viper.Set("monitor.writeInterval", time.Second*5)
+	config.Init()
+	config.Conf.Monitor.WriteInterval = time.Second * 10
+	log.ConfigLog()
+	db.PrepareConnection()
+	gin.SetMode(gin.ReleaseMode)
+	router = gin.New()
+	router.Use(log.GinLog())
+	var ctl rest.Restful
+	ctl.Init(router)
+	var pingCtl ping.Controller
+	pingCtl.Init(router)
+	monitor.StartMonitor()
+	m.Run()
+}
