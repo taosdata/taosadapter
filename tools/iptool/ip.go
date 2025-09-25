1737da9d9c62f5b92afd2721534b5e68d10ed497
t_max <1172915550@qq.com>
2025-09-25 19:25:42 +0800

    feat: enhance SQL recording to include application name and source port

--- tools/iptool/ip.go
+++ tools/iptool/ip.go
@@ -3,6 +3,7 @@ package iptool
 import (
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -22,4 +23,29 @@ func GetRealIP(r *http.Request) net.IP {
 		return nil
 	}
 	return ipAddr.IP
+}
+
+func GetRealPort(r *http.Request) (string, error) {
+	port := r.Header.Get("X-Real-Port")
+	if port != "" {
+		_, err := strconv.ParseUint(port, 10, 32)
+		if err != nil {
+			return "", err
+		}
+		return port, nil
+	}
+
+	_, port, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr))
+	if err != nil {
+		return "", err
+	}
+	if port != "" {
+		_, err = strconv.ParseUint(port, 10, 32)
+		if err != nil {
+			return "", err
+		}
+		return port, nil
+	}
+
+	return port, nil
 }
