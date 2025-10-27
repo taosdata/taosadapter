0d88abafc632973404baf862212895217ae55a39
Xuefeng Tan <1172915550@qq.com>
2025-10-27 14:49:23 +0800

    feat: convert Inf/NaN values to null in REST API responses (#448)
    * feat: convert Inf/NaN values to null in REST API responses
    
    * feat: refactor SQL test cases to use executeSQL helper function

--- plugin/influxdb/plugin_test.go
+++ plugin/influxdb/plugin_test.go
@@ -3,9 +3,11 @@ package influxdb
 import (
 	"database/sql/driver"
 	"fmt"
+	"math"
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -23,22 +25,39 @@ import (
 	"github.com/taosdata/taosadapter/v3/tools/testtools"
 )
 
-// @author: xftan
-// @date: 2021/12/14 15:07
-// @description: test influxdb plugin
-func TestInfluxdb(t *testing.T) {
-	//nolint:staticcheck
-	rand.Seed(time.Now().UnixNano())
+var router *gin.Engine
+
+func TestMain(m *testing.M) {
 	viper.Set("smlAutoCreateDB", true)
 	defer viper.Set("smlAutoCreateDB", false)
 	viper.Set("influxdb.enable", true)
 	config.Init()
 	log.ConfigLog()
 	db.PrepareConnection()
+	router = gin.Default()
+	p := Influxdb{}
+	err := p.Init(router)
+	if err != nil {
+		panic(err)
+	}
+	err = p.Start()
+	if err != nil {
+		panic(err)
+	}
+	code := m.Run()
+	err = p.Stop()
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(code)
+}
+
+// @author: xftan
+// @date: 2021/12/14 15:07
+// @description: test influxdb plugin
+func TestInfluxdb(t *testing.T) {
 	logger := log.GetLogger("test")
 	isDebug := log.IsDebug()
-	p := Influxdb{}
-	router := gin.Default()
 	conn, err := syncinterface.TaosConnect("", "root", "taosdata", "", 0, logger, isDebug)
 	if err != nil {
 		t.Error(err)
@@ -53,15 +72,7 @@ func TestInfluxdb(t *testing.T) {
 		err = exec(conn, "drop database if exists test_plugin_influxdb")
 		assert.NoError(t, err)
 	}()
-	err = p.Init(router)
-	assert.NoError(t, err)
-	err = p.Start()
-	assert.NoError(t, err)
 	number := rand.Int31()
-	defer func() {
-		err = p.Stop()
-		assert.NoError(t, err)
-	}()
 	w := httptest.NewRecorder()
 	reader := strings.NewReader(fmt.Sprintf("measurement,host=host1 field1=%di,field2=2.0,fieldKey=\"Launch 🚀\" %d", number, time.Now().UnixNano()))
 	req, _ := http.NewRequest("POST", "/write?u=root&p=taosdata&db=test_plugin_influxdb&app=test_influxdb", reader)
@@ -109,6 +120,50 @@ func TestInfluxdb(t *testing.T) {
 	if values[0][0].(int32) != 1000 {
 		t.Fatal("ttl miss")
 	}
+}
+
+func TestInfAndNaN(t *testing.T) {
+	logger := log.GetLogger("test")
+	isDebug := log.IsDebug()
+
+	conn, err := syncinterface.TaosConnect("", "root", "taosdata", "", 0, logger, isDebug)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer func() {
+		syncinterface.TaosClose(conn, logger, isDebug)
+	}()
+	err = exec(conn, "drop database if exists test_plugin_influxdb_inf_nan")
+	assert.NoError(t, err)
+	defer func() {
+		err = exec(conn, "drop database if exists test_plugin_influxdb_inf_nan")
+		assert.NoError(t, err)
+	}()
+	w := httptest.NewRecorder()
+
+	now := time.Now()
+	reader := strings.NewReader(fmt.Sprintf(
+		"measurement,host=host1 field1=1i,field2=2.0 %d\n"+
+			"measurement,host=host1 field1=1i,field2=nan %d\n"+
+			"measurement,host=host1 field1=1i,field2=inf %d",
+		now.UnixNano(),
+		now.Add(time.Second).UnixNano(),
+		now.Add(time.Second*2).UnixNano(),
+	))
+	req, _ := http.NewRequest("POST", "/write?u=root&p=taosdata&db=test_plugin_influxdb_inf_nan&app=test_influxdb", reader)
+	req.RemoteAddr = testtools.GetRandomRemoteAddr()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 204, w.Code)
+	var values [][]driver.Value
+	assert.Eventually(t, func() bool {
+		values, err = query(conn, "select * from test_plugin_influxdb_inf_nan.`measurement` order by _ts asc")
+		return err == nil && len(values) == 3
+	}, 2*time.Second, 200*time.Millisecond)
+
+	assert.Equal(t, float64(2), values[0][2].(float64))
+	assert.True(t, math.IsNaN(values[1][2].(float64)))
+	assert.True(t, math.IsInf(values[2][2].(float64), 1))
 }
 
 func exec(conn unsafe.Pointer, sql string) error {
